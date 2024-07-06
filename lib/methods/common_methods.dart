20c81270e8eedb49a833552b3f0476acde506cb7
MuhammedYahiya <yahiyahmhd@gmail.com>
2024-07-07 03:10:07 +0530

    Login and Logout creaed

---
+++ lib/methods/common_methods.dart
@@ -0,0 +1,21 @@
+import 'package:connectivity_plus/connectivity_plus.dart';
+import 'package:flutter/material.dart';
+
+class CommonMethods {
+  checkConnectivity(BuildContext context) async {
+    var connectionResult = await Connectivity().checkConnectivity();
+
+    if (!connectionResult.contains(ConnectivityResult.mobile) &&
+        !connectionResult.contains(ConnectivityResult.wifi)) {
+      if (!context.mounted) return;
+      displaySnackBar(
+          "Your Internet is not Available. Check your connection and try again",
+          context);
+    }
+  }
+
+  displaySnackBar(String messageText, BuildContext context) {
+    var snackBar = SnackBar(content: Text(messageText));
+    ScaffoldMessenger.of(context).showSnackBar(snackBar);
+  }
+}
